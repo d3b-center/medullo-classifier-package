7e597800eb4c60474c4df7dd469f86840d2256ad
komalsrathi <komal.rathi@gmail.com>
2019-10-15 14:30:05 -0400

    adds RData and removes RDS #5

---
+++ R/bestFeaturesNew.R
@@ -0,0 +1,7 @@
+#' Dataset required by medulloClassify
+#'
+#' This dataset contains a vector of 279 genes
+#'
+#'
+#' @format A character vector of 279 genes
+"bestFeaturesNew"
---
+++ R/medulloSetsUp.R
@@ -0,0 +1,13 @@
+#' Dataset required by medulloClassify
+#'
+#' This dataset contains a list of 4 Medulloblastoma subtypes with gene signatures
+#'
+#'
+#' @format A list of 4 Medulloblastoma subtypes with gene signatures
+#' \describe{
+#'   \item{WNT}{character vector of 60 genes}
+#'   \item{SHH}{character vector of 20 genes}
+#'   \item{Group3}{character vector of 158 genes}
+#'   \item{Group4}{character vector of 274 genes}
+#' }
+"medulloSetsUp"
