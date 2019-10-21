5dec36b0b815814b0c56543b8917996f504e79e5
komalsrathi <komal.rathi@gmail.com>
2019-10-21 11:57:43 -0400

    add example dataset

---
+++ R/exprs_109401.R
@@ -0,0 +1,8 @@
+#' Dataset required by medulloClassify
+#'
+#' This dataset contains an expression matrix where the rownames are HUGO gene
+#' symbols and the column names contain the sample identifiers. Dataset also contains a
+#' character vector containing the observations (i.e. actual classifications) made for each sample.
+#'
+#'
+"exprs_109401"
